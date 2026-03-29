994cc3859a2445473173f74a0666d9e72c44b8a6
Surya Pratap Singh Solanki <suryapratapsingh2027@gmail.com>
2026-03-29 11:11:09 +0530

    Change API base URL to production server

--- Frontend/src/interview/services/interview.api.js
+++ Frontend/src/interview/services/interview.api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const api = axios.create({
-   baseURL: 'http://localhost:3000',
+   baseURL: 'https://ai-resume-analyzer-h8rx.onrender.com',
     withCredentials: true
 })
 
